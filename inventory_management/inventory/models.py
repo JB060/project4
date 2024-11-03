39b2e09eed931eef5758721f85ec81d1a7c8b310
Jason Byrne <jasonbyrne060@gmail.com>
2024-11-03 17:18:52 +0000

    added the table and contents to the dashboard page ofr the application

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class InventoryItem(models.Model):
+    name = models.CharField(max_lenght=200)
+    quantity = models.IntergerField()
+    category = models.ForeignKey('category', on_delete=models.SET_Null, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name 
+
+
+class Category(models.Model):
+    name = models.CharField(max_lenght=200)
+
+    def __str__(self):
+        return self.name 
