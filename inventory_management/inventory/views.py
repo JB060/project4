227ec8f0fcbbad9d366a6da18500c085425966c4
Jason Byrne <jasonbyrne060@gmail.com>
2024-11-03 13:16:27 +0000

    setup all the django frmeworkd and

---
+++ inventory_management/inventory/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class Index(TemplateView):
+    template_name = 'inventory/index.html'
+
+
+# Create your views here.
