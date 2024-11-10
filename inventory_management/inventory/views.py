f623be9759302948730cda1b652276ae7918254e
Jason Byrne <jasonbyrne060@gmail.com>
2024-11-10 18:31:47 +0000

    added the row of 3 for teh

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -1,52 +1,70 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
-from django.contrib.auth import authenticate, login 
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm, InventoryItemForm
-from .models import InventoryItem,Category
+from .models import InventoryItem, Category
+from inventory_management.settings import LOW_QUANTITY
+from django.contrib import messages
 
 class Index(TemplateView):
-    template_name = 'inventory/index.html'
+	template_name = 'inventory/index.html'
 
 class Dashboard(LoginRequiredMixin, View):
-    def get(self, request):
-        items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
+	def get(self, request):
+		items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
 
-        return render(request, 'inventory/dashboard.html', {'items': items})
+		low_inventory = InventoryItem.objects.filter(
+			user=self.request.user.id,
+			quantity__lte=LOW_QUANTITY
+		)
+
+		if low_inventory.count() > 0:
+			if low_inventory.count() > 1:
+				messages.error(request, f'{low_inventory.count()} items have low inventory')
+			else:
+				messages.error(request, f'{low_inventory.count()} item has low inventory')
+
+		low_inventory_ids = InventoryItem.objects.filter(
+			user=self.request.user.id,
+			quantity__lte=LOW_QUANTITY
+		).values_list('id', flat=True)
+
+		return render(request, 'inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
 
 class SignUpView(View):
-    def get(self, request):
-        form = UserRegisterForm()
-        return render (request, 'inventory/signup.html', {'form': form})
+	def get(self, request):
+		form = UserRegisterForm()
+		return render(request, 'inventory/signup.html', {'form': form})
 
-    def post(self, request):
-        form = UserRegisterForm(request.POST)
+	def post(self, request):
+		form = UserRegisterForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            user = authenticate(
-                username=form.clean_data['username'],
-                password=form.clean_data['password1']
-            )
+		if form.is_valid():
+			form.save()
+			user = authenticate(
+				username=form.cleaned_data['username'],
+				password=form.cleaned_data['password1']
+			)
 
-            login (request, user)
-            return redirect ('index')
+			login(request, user)
+			return redirect('index')
 
-        return render (request, 'inventory/signup.html', {'form':form})
+		return render(request, 'inventory/signup.html', {'form': form})
 
 class AddItem(LoginRequiredMixin, CreateView):
-	    model = InventoryItem
-	    form_class = InventoryItemForm
-	    template_name = 'inventory/item_form.html'
-	    success_url = reverse_lazy('dashboard')
+	model = InventoryItem
+	form_class = InventoryItemForm
+	template_name = 'inventory/item_form.html'
+	success_url = reverse_lazy('dashboard')
 
-def get_context_data(self, **kwargs):
+	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		context['categories'] = Category.objects.all()
 		return context
 
-def form_valid(self, form):
+	def form_valid(self, form):
 		form.instance.user = self.request.user
 		return super().form_valid(form)
 
